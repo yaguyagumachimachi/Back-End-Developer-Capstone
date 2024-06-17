a07465805e912c42280834bb6a2387a8c013a03d
yaguyagumachimachi <117585160+yaguyagumachimachi@users.noreply.github.com>
2024-06-17 10:10:08 +0000

    add and fix  week2 files

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Menu(models.Model):
+    Title = models.CharField(max_length=255, unique=True)
+    Price = models.DecimalField(max_digits=6, decimal_places=2)
+    Inventory = models.SmallIntegerField()
+
+    # class Meta:
+    #     unique_together = ('Title', 'Price')
+
+
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.PositiveSmallIntegerField()
+    BookingDate = models.DateField()
+
+    # class Meta:
+    #     unique_together = ('Name', 'Price')
